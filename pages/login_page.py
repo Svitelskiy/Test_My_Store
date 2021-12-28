0123d6f0eade708ec3d499f6e53c67bd237b3c1e
Svitelskiy <85756206+Svitelskiy@users.noreply.github.com>
2021-12-28 12:19:29 +0200

    added navigate to sign in and login, password, eanter button

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,3 +8,8 @@ class LoginPage:
         self.login_input = self.browser.find_element(*LoginLocators.email_address_input)
         self.input_password = self.browser.find_element(*LoginLocators.password_input)
         self.button_enter = self.browser.find_element(*LoginLocators.enter_button)
+
+    def enter_valid_credentials(self, login, password):
+        self.login_input.send_keys(login)
+        self.input_password.send_keys(password)
+        self.button_enter.click()
--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,6 @@ class MainPage:
         self.browser = browser
 
         self.sign_in_button = self.browser.find_element(*MainPageLocators.sign_in_button)
+
+    def navigate_to_sign_in(self):
+        self.sign_in_button.click()
