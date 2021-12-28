72ad03ab76477d23c6c83c95e7a0bedd60528143
Svitelskiy <85756206+Svitelskiy@users.noreply.github.com>
2021-12-28 12:17:09 +0200

    added user

--- conftest.py
+++ conftest.py
@@ -4,9 +4,14 @@ from selenium import webdriver
 
 @pytest.fixture(scope="function")
 def browser():
-    browser = webdriver.Chrome(executable_path="C:\\Users\\admin\\PycharmProjects\\"
-                                               "Test_My_Store\\chromedriver_win32\\chromedriver.exe")
-    browser.get("http://automationpractice.com/index.php?id_category=4&controller=category")
-    browser.implicitly_wait(5)
-    yield browser
-    browser.quit()
+    b = webdriver.Chrome(executable_path="C:\\Users\\admin\\PycharmProjects\\"
+                                         "Test_My_Store\\chromedriver_win32\\chromedriver.exe")
+    b.get("https://automationpractice.com/index.php?id_category=4&controller=category")
+    b.implicitly_wait(5)
+    yield b
+    b.quit()
+
+
+def user():
+    return {"login": "sashko@gmail.com",
+            "password": "qwe123qwer"}
